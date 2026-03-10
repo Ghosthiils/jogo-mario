04e744d503873b43e16990980c2dbe9042c555b0
Luiz Fernando <luizfranca35mc@gmail.com>
2026-03-09 21:55:54 -0300

    criando a APIs da pontuacao
    Ainda estou criando as APIs, pouco a pouco

--- server.js
+++ server.js
@@ -31,6 +31,22 @@ app.get('/', (req, res) => {
     res.send('deu bom')
 })
 
+fetch('/salvar-pontuacao')
+
+app.post('/salvar-pontuacao', (req, res) => {
+    const {nome, pontuacao} = req.body
+
+    const sql = "INSERT INTO jogadores (nome, pontos) VALUES (?, ?)"
+
+    db.query(sql, [nome, pontos], (err, result) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({erro: "Erro ao salvar pontuação"})
+        }
+        res.json({mensagem: "Pontuação salva com sucesso"})
+    })
+})
+
 
 
 app.listen(process.env.PORT,  () => {
